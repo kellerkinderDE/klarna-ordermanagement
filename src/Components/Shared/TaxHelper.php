30aa42ea699a90848f26acd00aa0f4637f481052
Ralf Nitzer <Ralf.nitzer@bestit-online.de>
2019-05-19 17:54:23 +0200

    KP-51 Add Tax Helper to determine if the user is allowed free tax order

---
+++ src/Components/Shared/TaxHelper.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace BestitKlarnaOrderManagement\Components\Shared;
+
+/**
+ * Class to find out if user is allowed for tax free order.
+ *
+ * @package BestitKlarnaOrderManagement\Components\Shared
+ *
+ */
+class TaxHelper
+{
+    /** @var array */
+    protected $userData = [];
+
+    /**
+     * @param array $userData
+     *
+     * @return void
+     */
+    public function setUserdata(array $userData)
+    {
+        $this->userData = $userData;
+    }
+
+    /**
+     * Validates if the provided customer should get a tax free delivery
+     *
+     * @return bool
+     */
+    public function isTaxFreeDelivery()
+    {
+        if (empty($this->userData)) {
+            return false;
+        }
+
+        if (!empty($this->userData['additional']['countryShipping']['taxfree'])) {
+            return true;
+        }
+
+        if (empty($this->userData['additional']['countryShipping']['taxfree_ustid'])) {
+            return false;
+        }
+
+        if (empty($this->userData['shippingaddress']['ustid']) &&
+            !empty($this->userData['billingaddress']['ustid']) &&
+            !empty($this->userData['additional']['country']['taxfree_ustid'])) {
+            return true;
+        }
+
+        return !empty($this->userData['shippingaddress']['ustid']);
+    }
+}
