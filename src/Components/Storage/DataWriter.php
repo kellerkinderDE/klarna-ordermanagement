4bd35991e1b26ed5f5925fe612398064f5eb7c1d
Steffen Beisenherz <s.beisenherz@kellerkinder.de>
2021-09-05 14:09:58 +0200

    [KLARNASUPPORT-184] Add order history entries during klarna actions

--- src/Components/Storage/DataWriter.php
+++ src/Components/Storage/DataWriter.php
@@ -2,6 +2,7 @@
 
 namespace BestitKlarnaOrderManagement\Components\Storage;
 
+use DateTime;
 use Doctrine\DBAL\Connection;
 
 /**
@@ -35,6 +36,29 @@ class DataWriter
         if (empty($transactionId)) {
             return 0;
         }
-        return $this->connection->update('s_order', ['cleared' => $statusId], ['transactionID' => $transactionId]);
+
+        /** @var array $order */
+        $order = $this->connection->createQueryBuilder()
+            ->addSelect('o.*')
+            ->from('s_order', 'o')
+            ->andWhere('transactionID = :transactionId')
+            ->setParameter('transactionId', $transactionId)
+            ->execute()
+            ->fetch();
+
+        $orderStatus = $order['status'];
+
+        $this->connection->update('s_order', ['cleared' => $statusId], ['transactionID' => $transactionId]);
+        return $this->connection->insert(
+            's_order_history',
+            [
+                'orderID' => $order['id'],
+                'previous_order_status_id' => $orderStatus,
+                'order_status_id' => $orderStatus,
+                'previous_payment_status_id' => $order['cleared'],
+                'payment_status_id' => $statusId,
+                'change_date' => (new DateTime())->format('Y-m-d H:i:s')
+            ]
+        );
     }
 }
